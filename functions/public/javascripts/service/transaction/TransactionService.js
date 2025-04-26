6a6f29a5da294dcf25dcda37e178a4a5454f701b
Zidanbz <zidanbz03@gmail.com>
2025-04-26 11:15:13 +0800

    Fix

--- functions/public/javascripts/service/transaction/TransactionService.js
+++ functions/public/javascripts/service/transaction/TransactionService.js
@@ -33,27 +33,26 @@ async function dataTransaction(req){
             let discount;
             discount = (voucher[0].piece / 100) * transactional.price;
             discount = Math.min(discount, transactional.price);
-            price = transactional.price - discount;
+            price = Number(transactional.price - discount);
         }else {
-            price = transactional.price;
+            price = Number(transactional.price);
         }
-        if (project.materialName.length > 50){
-            name = "Name Is Not Defined";
-        }else {
-            name = project.name;
-        }
-
+        if (!project.materialName || project.materialName.length > 50) {
+    name = "Name Is Not Defined";
+}else {
+    name = project.materialName;
+}
         const data = {
             "transaction_details": {
                 order_id: transactional.ID,
                 gross_amount: price,
             },
-            "item_details": {
+            "item_details": [{
                 "id": project.ID,
                 "price": price,
                 "quantity": 1,
                 "name": name,
-            },
+            }],
             "customer_details": {
                 "email": transactional.email,
             },
@@ -67,6 +66,7 @@ async function dataTransaction(req){
 async function newTransaction(req){
     try {
         const {data, transactional} = await dataTransaction(req);
+        console.log("Data dikirim ke Midtrans:", JSON.stringify(data, null, 2)); // ❌
         await createNewTransaction(transactional);
             const response = await axios.post(
                 "https://app.sandbox.midtrans.com/snap/v1/transactions",
